d1eb8b6c9c1d3a0ba721fd5241a1873bcf87403f
Robert Graff <robert@workingdemo.com>
2023-10-02 04:48:04 +0000

    embedded arrays

--- lib/change_builders/full_diff.ex
+++ lib/change_builders/full_diff.ex
@@ -1,3 +1,4 @@
+
 defmodule AshPaperTrail.Dumpers.FullDiff do
   def build_changes(attributes, changeset) do
     Enum.reduce(attributes, %{}, &build_attribute_change(&1, changeset, &2))
@@ -19,23 +20,75 @@ defmodule AshPaperTrail.Dumpers.FullDiff do
     end
   end
 
-  defp build_attribute_change(
-         :embedded_array,
-         %{type: {:array, _type}} = attribute,
-         changeset,
-         changes
-       ) do
-    if changeset.action_type == :create do
+  defp build_attribute_change(:embedded_array, attribute, changeset, changes) do
+    # Get the primary keys
+    {:array, resource} = attribute.type
+    primary_keys = Ash.Resource.Info.primary_key(resource)
+
+    # Get the datas (changing from)
+    {uniq_keys, datas, data_indexes} = Ash.Changeset.get_data(changeset, attribute.name) 
+    |> dump_value(attribute)
+    |> List.wrap() 
+    |> Enum.with_index(& {&2, &1})
+    |> Enum.reduce({MapSet.new(), %{}, %{}}, fn {index, data}, {uniq_keys, datas, data_indexes} ->
+      keys = map_get_keys(data, primary_keys)
+      {MapSet.put(uniq_keys, keys), Map.put(datas, keys, data), Map.put(data_indexes, keys, index)}
+    end)
+
+    # Get the values (changing to)
+    {uniq_keys, values, value_indexes} = case Ash.Changeset.fetch_change(changeset, attribute.name) do
+     {:ok, values} -> values
+     :error -> []
+    end 
+    |> dump_value(attribute)
+    |> Enum.with_index(& {&2, &1})
+    |> Enum.reduce({uniq_keys, %{}, %{}}, fn {index, value}, {uniq_keys, values, value_indexes} ->
+      keys = map_get_keys(value, primary_keys)
+      {MapSet.put(uniq_keys, keys), Map.put(values, keys, value), Map.put(value_indexes, keys, index)}
+    end)    
+
+    # Build a change for each id
+    {changed?, embedded_changes} = Enum.reduce(uniq_keys, {false, []}, fn key, {changed?, embedded_changes} -> 
+      data = Map.get(datas, key)
+      value = Map.get(values, key)
+
+      index_change = dump_change_value(
+            Map.has_key?(data_indexes, key),
+            Map.get(data_indexes, key),
+            Map.has_key?(value_indexes, key),
+            Map.get(value_indexes, key)
+          )
+
+      case build_map_changes(data, value) do 
+        %{unchanged: _unchanged} = change ->
+          change = Map.put(change, :index, index_change)
+          {changed?, [change | embedded_changes ]}
+
+        %{} = change ->
+          change = Map.put(change, :index, index_change)
+          {true, [change | embedded_changes]}
+      end
+    end)
+
+    # Sort them by [current_index, 1] if present, [datas_index, 0] previous index (if removed)
+    embedded_changes = Enum.sort_by(embedded_changes, fn change -> 
+      case change do
+        %{destroyed: _embedded, index: %{from: i}} -> [i, 0]
+        %{index: %{to: i}} -> [i, 1]
+      end
+    end)
+
+    if ((changeset.action_type == :create) || changed?) do
       Map.put(
         changes,
         attribute.name,
-        %{to: []}
+        %{to: embedded_changes}
       )
     else
       Map.put(
         changes,
         attribute.name,
-        %{unchanged: []}
+        %{unchanged: embedded_changes}
       )
     end
   end
@@ -106,8 +159,12 @@ defmodule AshPaperTrail.Dumpers.FullDiff do
     %{destroyed: dump_map_changes(data, %{})}
   end
 
+  defp build_map_changes(data, data) do
+    %{unchanged: dump_map_changes(data, data)}
+  end
+
   defp build_map_changes(data, value) do
-    %{update: dump_map_changes(data, value)}
+    %{updated: dump_map_changes(data, value)}
   end
 
   defp dump_map_changes(%{} = from_map, %{} = to_map) do
@@ -135,5 +192,9 @@ defmodule AshPaperTrail.Dumpers.FullDiff do
   defp dump_value(value, attribute) do
     {:ok, dumped_value} = Ash.Type.dump_to_embedded(attribute.type, value, [])
     dumped_value
+  end
+
+  defp map_get_keys(resource, keys) do
+    Enum.map(keys, &Map.get(resource, &1))
   end
 end
