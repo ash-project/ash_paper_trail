198e8fa131b13bd119262e0d1fbb28f837021e70
Tun Cham Roeun <chamroeuntun25@outlook.com>
2024-06-13 19:21:06 +0700

    fix: Check if `Ash.Domain` requires authorization (#82)
    * Check if `Ash.Domain` requires authorization
    
    * improvement: set context that can be used in policies
    
    ---------
    
    Co-authored-by: Zach Daniel <zachary.s.daniel@gmail.com>

--- test/support/articles/domain.ex
+++ test/support/articles/domain.ex
@@ -2,6 +2,10 @@ defmodule AshPaperTrail.Test.Articles.Domain do
   @moduledoc false
   use Ash.Domain, extensions: [AshPaperTrail.Domain], validate_config_inclusion?: false
 
+  authorization do
+    authorize(:always)
+  end
+
   resources do
     resource AshPaperTrail.Test.Articles.Article
   end
